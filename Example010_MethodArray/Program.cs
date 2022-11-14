eab61dd05b39d54c8ed54c36994eb39d40bbd3fa
HumanBox <stashksenia@gmail.com>
2022-11-14 11:55:44 +0300

    MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 111, 52, 31, 4, 18, 16, 7, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // Прерывает работу программы при первом же совпадении условий поиска
+    }
+    index++;
+    // index = index + 1
+    // index +=1;
+}
