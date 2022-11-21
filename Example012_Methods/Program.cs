cf2228ba60926092a979bb9ebee31c5181900c81
HumanBox <stashksenia@gmail.com>
2022-11-21 11:44:29 +0300

    Sort

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -87,3 +87,73 @@ for(int i = 2; i <= 10; i++)
     }
     Console.WriteLine(); // Сделает "дырку" между выводами
 }
+
+// =======Работа с текстом
+// Дан текст. В тексте нужно заменить все пробелы чёрточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+/// strins s = "qwerty"
+//              012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue) // char - символ
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+
+int[] arr ={1, 5, 4, 8, 9, 3, 6, 1, 3, 8};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+     for(int i = 0; i < count; i++)   
+     {
+        Console.Write($"{array[i]} ");
+     } 
+     Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    } 
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
